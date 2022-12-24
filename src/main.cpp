6feea789ae1cd81bf4d878a92abb13122aa9f32f
Naim Masri <naeemmas@gmail.com>
2022-12-24 15:43:10 +0300

    init commit

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+
+// TODO:  LoRa communication codes
+// TODO:  GPS reading codes
+// TODO:  Compass reading codes
+// TODO:  IMU reading codes
+// TODO:  IMU + Compass fusion
+// TODO:  Motor control codes
+// TODO:  RC remote control 
+// TODO:  GPS guiding codes
+// TODO:  GS communication codes
+
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}

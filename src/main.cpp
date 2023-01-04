1845936ec8dd2a5060fa475f36af26467e4cae92
Naim Masri <naeemmas@gmail.com>
2023-01-04 16:03:38 +0300

    Add car forward and backward control

--- src/main.cpp
+++ src/main.cpp
@@ -10,20 +10,20 @@
 // TODO:  GPS guiding codes
 // TODO:  GS communication codes
 
-#define M_1_PINB GPIO_NUM_4  // Backword pin
-#define M_1_PINF GPIO_NUM_16 // Forword pin
+#define M_1_PINB GPIO_NUM_4  // Backward pin
+#define M_1_PINF GPIO_NUM_16 // Forward pin
 #define M_1_PINP GPIO_NUM_17 // PWM pin
 
-#define M_2_PINB GPIO_NUM_18 // Backword pin
-#define M_2_PINF GPIO_NUM_19 // Forword pin
+#define M_2_PINB GPIO_NUM_18 // Backward pin
+#define M_2_PINF GPIO_NUM_19 // Forward pin
 #define M_2_PINP GPIO_NUM_21 // PWM pin
 
-#define M_3_PINB GPIO_NUM_26 // Backword pin
-#define M_3_PINF GPIO_NUM_27 // Forword pin
+#define M_3_PINB GPIO_NUM_26 // Backward pin
+#define M_3_PINF GPIO_NUM_27 // Forward pin
 #define M_3_PINP GPIO_NUM_13 // PWM pin
 
-#define M_4_PINB GPIO_NUM_32 // Backword pin
-#define M_4_PINF GPIO_NUM_33 // Forword pin
+#define M_4_PINB GPIO_NUM_32 // Backward pin
+#define M_4_PINF GPIO_NUM_33 // Forward pin
 #define M_4_PINP GPIO_NUM_25 // PWM pin
 
 #define PWM_FREQ 5000
@@ -40,9 +40,12 @@
 #define M4_INDEX 3
 #define MOTORS_COUNT 4
 
-extern void motorBackword(int motorIndex, int speed);
-extern void motorForword(int motorIndex, int speed);
+extern void motorBackward(int motorIndex, int speed);
+extern void motorForward(int motorIndex, int speed);
 extern void motorStop(int motorIndex);
+extern void carForward(int speed);
+extern void carBackward(int speed);
+extern void stopCar();
 
 int motorFpins[MOTORS_COUNT] = {M_1_PINF,
                               M_2_PINF,
@@ -71,26 +74,17 @@ void setup()
 }
 
 void loop()
-{
- for (size_t i = 0; i < MOTORS_COUNT; i++)
- {
-  motorForword(i, 50);
- }
- delay(10000);
- for (size_t i = 0; i < MOTORS_COUNT; i++)
- {
-  motorStop(i);
- }
-delay(5000);
+{ 
+
 }
 
-void motorBackword(int motorIndex, int speed)
+void motorBackward(int motorIndex, int speed)
 {
   digitalWrite(motorBpins[motorIndex],HIGH);
   digitalWrite(motorFpins[motorIndex],LOW);
   ledcWrite(motorIndex, PCT_TO_PWM(speed));
 }
-void motorForword(int motorIndex, int speed)
+void motorForward(int motorIndex, int speed)
 {
   digitalWrite(motorBpins[motorIndex],LOW);
   digitalWrite(motorFpins[motorIndex],HIGH);
@@ -101,4 +95,38 @@ void motorStop(int motorIndex)
   digitalWrite(motorBpins[motorIndex],HIGH);
   digitalWrite(motorFpins[motorIndex],HIGH);
   ledcWrite(motorIndex, 0);
+}
+
+void carForward(int speed)
+{
+  stopCar();
+  for (size_t i = 0; i < MOTORS_COUNT; i++)
+  {
+    ledcWrite(i, PCT_TO_PWM(speed));
+  }
+  digitalWrite(motorBpins[M1_INDEX], LOW);
+  digitalWrite(motorBpins[M2_INDEX], LOW);
+  digitalWrite(motorBpins[M3_INDEX], LOW);
+  digitalWrite(motorBpins[M4_INDEX], LOW);
+}
+
+void carBackward(int speed)
+{
+  stopCar();
+  for (size_t i = 0; i < MOTORS_COUNT; i++)
+  {
+    ledcWrite(i, PCT_TO_PWM(speed));
+  }
+  digitalWrite(motorFpins[M1_INDEX], LOW);
+  digitalWrite(motorFpins[M2_INDEX], LOW);
+  digitalWrite(motorFpins[M3_INDEX], LOW);
+  digitalWrite(motorFpins[M4_INDEX], LOW);
+}
+
+void stopCar()
+{
+  for (size_t i = 0; i < MOTORS_COUNT; i++)
+  {
+    motorStop(i);
+  } 
 }
